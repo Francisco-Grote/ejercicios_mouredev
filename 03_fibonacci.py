27997338422fb0dbb7b36725311e8206fef7f2f0
fperezgrote <fperezgrote@Z-00100>
2025-08-14 08:49:01 -0300

    push ejercicio 3

---
+++ 03_fibonacci.py
@@ -0,0 +1,19 @@
+"""
+#2-LA SUCESIÓN DE FIBONACCI
+/*
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
+"""
+
+cero = 0
+uno = 1
+
+for i in range(0,50):
+    print(cero)
+    fib = cero + uno
+    cero = uno
+    uno = fib
